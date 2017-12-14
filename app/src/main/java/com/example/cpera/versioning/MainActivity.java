38f4b4bfeb0ba2eb0848a9a5530ddc04ba2c840a
venaseph <cperagine@gmail.com>
2017-12-13 22:48:37 -0500

    inProgress

--- app/src/main/java/com/example/cpera/versioning/MainActivity.java
+++ app/src/main/java/com/example/cpera/versioning/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.cpera.versioning;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,86 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // in onCreate, will not run when app is switched back to
+
+        String version = "";
+        try {
+            version = getAndroidVersion();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        TextView tv = findViewById(R.id.tvid);
+        tv.setText(version);
+        // tv.setText(R.string.outdated);
+
+
+    }
+
+    private String getAndroidVersion() {
+        String release = Build.VERSION.RELEASE;
+        int sdkVersion = Build.VERSION.SDK_INT;
+        return "Android SDK: " + sdkVersion + " (" + release +")";
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// test
